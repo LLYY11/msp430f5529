7d36c37ade1ec47272b03645a49d8bea37b3e063
zyhuang <1099290849@qq.com>
2019-10-11 13:17:29 +0800

    Update main.c

--- main.c
+++ main.c
@@ -5,12 +5,10 @@ int main()
 {
   // Stop watchdog timer to prevent time out reset
 WDTCTL = WDTPW + WDTHOLD;
-LCD_Init();
+LCD_Init();//oled12864初始化
 
 unsigned char str2[] = "https://www.baidu.com";
-P2DIR |= BIT1;
-P1DIR |= BIT1;
-LCD_ShowString(1,1,str2,16);
+LCD_ShowString(1,1,str2,16);//(坐标，坐标，字符串，字符大小)
 
 
 }
